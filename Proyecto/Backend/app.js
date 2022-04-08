48d6cf049535527e979130632d6b9e38816c15e8
Diego Daza <100430448+DiegoDaza07@users.noreply.github.com>
2022-04-08 14:28:46 -0300

    Add files via upload

---
+++ Proyecto/Backend/app.js
@@ -0,0 +1,31 @@
+
+  document.getElementById("open").addEventListener('click', () => {
+    document.getElementById("modal_container").classList.add('show');  });
+    document.getElementById("close").addEventListener('click', () => {
+    document.getElementById("modal_container").classList.remove('show');});
+  
+  
+    document.getElementById("open1").addEventListener('click', () => {
+      document.getElementById("modal_container1").classList.add('show');  });
+      document.getElementById("close1").addEventListener('click', () => {
+      document.getElementById("modal_container1").classList.remove('show');});
+      
+      document.getElementById("open2").addEventListener('click', () => {
+        document.getElementById("modal_container2").classList.add('show');  });
+        document.getElementById("close2").addEventListener('click', () => {
+        document.getElementById("modal_container2").classList.remove('show');});
+  
+        document.getElementById("open3").addEventListener('click', () => {
+          document.getElementById("modal_container3").classList.add('show');  });
+          document.getElementById("close3").addEventListener('click', () => {
+          document.getElementById("modal_container3").classList.remove('show');});
+  
+          document.getElementById("open4").addEventListener('click', () => {
+            document.getElementById("modal_container4").classList.add('show');  });
+            document.getElementById("close4").addEventListener('click', () => {
+            document.getElementById("modal_container4").classList.remove('show');});
+  
+  
+  
+  
+ 
